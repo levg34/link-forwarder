890fe8daa9169252c35d959da922b920011163ae
levg34 <levg34@users.noreply.github.com>
2024-01-18 20:00:59 +0000

    connect by flashing QR code

--- src/routes/connect.tsx
+++ src/routes/connect.tsx
@@ -1,10 +1,18 @@
+import { useSearchParams } from '@solidjs/router'
+import { Show } from 'solid-js'
 import QrCode from '~/components/QrCode'
 
 export default function Connect(props: { uuid: string }) {
+    const [searchParams] = useSearchParams()
+
+    const uuid = searchParams.uuid
+
     return (
         <>
-            <h3>Scan this code to connect.</h3>
-            <QrCode text={window.location.href + '?uuid=' + props.uuid} />
+            <Show when={!uuid} fallback={<p>Connected with {uuid}</p>}>
+                <h3>Scan this code to connect.</h3>
+                <QrCode text={window.location.href + '?uuid=' + props.uuid} />
+            </Show>
         </>
     )
 }
