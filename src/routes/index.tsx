ebad78a0805444d577b757df39f31e5b9e8ba726
levg34 <levg34@users.noreply.github.com>
2024-01-18 19:12:21 +0000

    add input

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,25 @@
 // import "./index.css";
 
+import { Show, createSignal } from 'solid-js'
 import QrCode from '~/components/QrCode'
 
 export default function Home() {
+    const [text, setText] = createSignal<string>('Test text')
     return (
         <main>
             <h1>Hello world!</h1>
-            <QrCode text="Test text" />
+            <div>
+                <input
+                    type="text"
+                    value={text()}
+                    oninput={(e) => {
+                        setText(e.target.value)
+                    }}
+                />
+            </div>
+            <Show when={text()}>
+                <QrCode text={text()} />
+            </Show>
         </main>
     )
 }
