6bc6f2b6031b1f8ab5e2e2a424e3209aae3c6fd6
levg34 <levg34@users.noreply.github.com>
2024-01-18 18:12:43 +0000

    add prettierrc and cosmetic changes

--- src/app.tsx
+++ src/app.tsx
@@ -9,7 +9,7 @@ export default function App() {
     <Router
       root={(props) => (
         <MetaProvider>
-          <Title>SolidStart - with Vitest</Title>
+          <Title>Link forwarder</Title>
           <Suspense>{props.children}</Suspense>
         </MetaProvider>
       )}
